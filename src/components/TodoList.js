ca6c6471ff347f6c104e53c0869cae16bf022e95
jonyflores <jonatan.flores@rappi.com>
2023-02-14 15:58:12 -0300

    react tutorial

---
+++ src/components/TodoList.js
@@ -0,0 +1,14 @@
+import Todo from "./Todo";
+
+const TodoList = ({ todoList, setTodoList}) => {
+    return (<div>
+        {todoList.map((todoItem) =>
+            <Todo key={todoItem.id} 
+            todoItem={todoItem} 
+            todoList={todoList}
+            setTodoList={setTodoList}/>
+        )}
+    </div>)
+}
+
+export default TodoList;
